576fb885e7a886ed8423f415f5306f9422d67430
Mohanapriyaa <priyaannakodi1996@gmail.com>
2016-08-08 11:57:43 +0530

    Add files via upload

---
+++ Arrsub.java
@@ -0,0 +1,35 @@
+
+public class Arrsub {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int a[]={4,6,9,8,5};
+		    int b[]={9,6,4};
+		    int n1=a.length;
+		    int n2=b.length;
+		    int c=0;
+		    for(int i=0;i<n1;i++)
+		    {
+		    	for(int j=0;j<n2;j++)
+		    	{
+		    	if(a[i]==b[j])
+		    	{
+		    	c++;
+			}
+		    	}
+		    }
+		    if(c==n1)
+		    {
+		    	System.out.println("b is subset of a");
+		    }
+		    else
+		    {
+		    	System.out.println("b is not subset of a");
+		    }
+
+	}
+
+}
